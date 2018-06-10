b25f4981c3a5e0069e448144dd2433959f7ed066
SwaggersaurusRex <lee.jeremy1@gmail.com>
2018-06-10 15:52:55 -0700

    Removed unnecessary imports.

--- src/test/java/PageObjects/LoginPage.java
+++ src/test/java/PageObjects/LoginPage.java
@@ -2,8 +2,6 @@ package PageObjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
     private WebDriver driver;
